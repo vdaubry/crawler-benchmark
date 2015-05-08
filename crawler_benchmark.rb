0806d1a480bcd51216c2cd0fae839d1a99485131
Vincent Daubry <vdaubry@gmail.com>
2015-05-08 10:52:41 +0200

    initialize sidekiq with remote redis in production

--- crawler_benchmark.rb
+++ crawler_benchmark.rb
@@ -4,8 +4,11 @@ require 'mechanize'
 require 'sidekiq'
 require 'sidekiq/api'
 
+Dotenv.load
+
 #Initializers
 require "initializers/redis"
+require "initializers/sidekiq"
 #Models
 require "models/crawler"
 #Workers
