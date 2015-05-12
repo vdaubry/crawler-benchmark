3f582a435db7707d9b50afaefa561e11c5a01ef4
Vincent Daubry <vdaubry@gmail.com>
2015-05-12 17:48:44 +0200

    use async dns resolve

---
+++ ruby/event_machine.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+require 'eventmachine'
+require 'em-http-request'
+require 'rubydns'
+require 'rubydns/system'
+require 'json'
+
+class MeasureDomain
+  def initialize(urls:)
+    @urls = urls
+    @response_count = 0
+    @resolver = RubyDNS::Resolver.new(RubyDNS::System::nameservers)
+  end
+  
+  def get_url(url:)
+    puts "GET #{url}"
+    @resolver.query(URI.parse(url).host) do |response|
+      if response.answer.empty?
+        p "Couldn't resolve hostname for #{url}" 
+        @response_count += 1
+      else
+        ip = response.answer[0][2].address.to_s
+        host = response.answer[0][0].to_s
+        
+        domain = Addressable::URI.parse(url)
+        domain.host = ip
+        
+        puts "get #{ip}, for #{url}"
+        page = EventMachine::HttpRequest.new(domain, :connect_timeout => 15).get(:head =>{'host' => host})
+        page.errback { 
+          p "Couldn't get #{url}" 
+          @response_count += 1
+        }
+        page.callback {
+          @response_count += 1
+          puts "Got response from #{url} : #{page.response.size} bytes"
+        }
+      end
+    end
+  end
+  
+  def start
+    EM.run {
+      n=0
+      EM.error_handler{ |e|
+        puts "Error raised during event loop: #{e.message}"
+        EM.stop
+      }
+      
+      EM::PeriodicTimer.new(0.1) do
+        if n < @urls.size
+          get_url(url: @urls[n])
+          n+=1
+        end
+        
+        EM.stop if @response_count >= @urls.count
+      end
+    }
+  end
+end
+
+puts "Loading urls from JSON"
+file = File.open("#{File.expand_path(File.dirname(__FILE__))}/../ressources/domains.json")
+urls = JSON.parse(file.read)["domains"][0..1000]
+
+start_time = Time.now.to_f
+MeasureDomain.new(urls: urls).start
+total_time  = Time.now.to_f - start_time
+puts "Benchmark finished : called #{urls.count} urls in #{total_time} seconds"
