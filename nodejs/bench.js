1e3d7db51ac470b546d323ba34f1ebfcc4a04c12
Vincent Daubry <vdaubry@gmail.com>
2015-05-13 10:56:37 +0200

    add node benchmark

---
+++ nodejs/bench.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var request = require('request');
+
+var delay = 100;
+function waitForUrlComplete() {    
+  setTimeout(function() {
+    if(i < urls.length) {
+      waitForUrlComplete();
+    }
+    else {
+      var total_time = Date.now() - start_time;
+      console.log("total time = "+total_time/1000.0+" seconds");
+    }
+  }, delay);
+}
+
+// Load json from file
+var obj = JSON.parse(fs.readFileSync('../ressources/domains.json', 'utf8'));
+var urls = obj['domains'].slice(0, 1000);
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
+
+var i=0;
+for (var url in urls) {
+  url = obj['domains'][url];
+  
+  console.log("GET "+url);
+  request(url, function (error, response, body) {
+    
+    if (!error && response.statusCode == 200) {
+      console.log("Done, size = "+body.length);
+      i+=1;
+    }
+    else {
+      console.log("GET request error : "+error);
+      console.log("GET request status : "+response.statusCode);
+      i+=1;
+    }
+  });
+}
+
+
+var start_time = Date.now();
+waitForUrlComplete();
