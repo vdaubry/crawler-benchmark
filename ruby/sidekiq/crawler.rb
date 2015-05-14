08b3ddae537893fe90b57ffdba2342afe924639a
Vincent Daubry <vdaubry@gmail.com>
2015-05-14 09:54:30 +0200

    add sidekiq benchmark

---
+++ ruby/sidekiq/crawler.rb
@@ -0,0 +1,43 @@
+require 'sidekiq'
+require 'sidekiq/api'
+require 'json'
+require_relative 'fetch_url_worker'
+
+class Crawler
+  def initialize(website_number:)
+    Sidekiq::Queue.new("crawler").clear
+    @website_number = (website_number || 5000).to_i
+  end
+  
+  def jobs_count
+    Sidekiq::Queue.new("crawler").size+Sidekiq::ProcessSet.new("crawler").size-1
+  end
+  
+  def measure
+    start_time = Time.now
+    puts "Crawling #{@website_number} domains"
+    
+    yield
+    
+    end_time = Time.now
+    puts "Done"
+    puts "total time = #{end_time - start_time}"
+  end
+    
+  def start
+    measure do
+      file = File.open("#{File.expand_path(File.dirname(__FILE__))}/../../ressources/domains-fast.json")
+      urls = JSON.parse(file.read)["domains"][0..@website_number]
+      puts "Loading #{urls.count} urls from JSON"
+      urls.each do |url|
+        FetchUrlWorker.perform_async(url)
+      end
+
+      loop do
+        break if jobs_count==0
+      end
+    end
+  end
+end
+
+Crawler.new(website_number: ARGV[0]).start
---
+++ ruby/sidekiq/fetch_url_worker.rb
@@ -0,0 +1,23 @@
+# Start sidekiq with : sidekiq -c 40 -r ./ruby/sidekiq/fetch_url_worker.rb
+
+require 'sidekiq'
+require 'mechanize'
+require 'timeout'
+require 'resolv-replace'
+
+class FetchUrlWorker
+  include Sidekiq::Worker
+   sidekiq_options :queue => :crawler, :retry => false, :backtrace => true
+  
+  def perform(url)
+    puts "Get #{url}"
+    begin
+      agent = Mechanize.new
+      agent.open_timeout=5
+      agent.read_timeout=5
+      Timeout::timeout(10) {agent.get(url)}
+    rescue StandardError => e
+      puts "Couldn't get #{url} : #{e}"
+    end
+  end
+end
